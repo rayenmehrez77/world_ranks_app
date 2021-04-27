1338b5faf943aab7658d14f17fc3ff0acc436f4e
rayenmehrez77 <rmehrez.555@gmail.com>
2021-04-27 02:09:33 +0200

    first commit

---
+++ src/pages/country/[id].js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from "../../components/Layout/Layout";
+
+
+const Country = ({ country  }) => {
+    return (
+        <Layout>
+           
+        </Layout>
+    )
+}
+
+export default Country 
